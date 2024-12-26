8f69e961c6ab815417cfc3d7e019d5764c3969dd
adam.zowde <adam.zowde@runi.ac.il>
2024-12-26 16:37:38 +0200

    handel the primary key

--- src/main/java/smarticulous/Smarticulous.java
+++ src/main/java/smarticulous/Smarticulous.java
@@ -90,9 +90,9 @@ public class Smarticulous {
              
              st.executeUpdate("CREATE TABLE IF NOT EXISTS User (UserId INTEGER PRIMARY KEY , Username TEXT UNIQUE , Firstname TEXT, Lastname TEXT,Password Text); ");
              st.executeUpdate("CREATE TABLE IF NOT EXISTS Exercise (ExerciseId INTEGER PRIMARY KEY , Name TEXT,DueDate INTEGER);");
-             st.executeUpdate("CREATE TABLE IF NOT EXISTS QUESTION (ExerciseId INTEGER, QuestionId INTEGER ,Name TEXT,Desc TEXT,Points INTEGER);");
+             st.executeUpdate("CREATE TABLE IF NOT EXISTS QUESTION (ExerciseId INTEGER, QuestionId INTEGER ,Name TEXT,Desc TEXT,Points INTEGER,PRIMARY KEY (ExerciseId,QuestionId) );");
              st.executeUpdate("CREATE TABLE IF NOT EXISTS Submission (SubmissionId INTEGER PRIMARY KEY , UserId TEXT , ExerciseId INTEGER,SubmissionTime INTEGER);");
-             st.executeUpdate("CREATE TABLE IF NOT EXISTS QuestionGrade(SubmissionId INTEGER , QuestionId INTEGER ,GRADE REAL);");
+             st.executeUpdate("CREATE TABLE IF NOT EXISTS QuestionGrade(SubmissionId INTEGER , QuestionId INTEGER ,GRADE REAL,PRIMARY KEY(SubmissionId,QuestionId);");
        
         return db;
     }
